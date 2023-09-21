920d5359c827ec405c9084c266cff19397e0c923
OskaNiiranen <oska.niiranen@gmail.com>
2023-09-21 11:00:33 +0300

    Hello Hello Hello.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-);
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  );
+};
 
 export default App;
